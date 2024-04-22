bc85e1bcc577367f58040baf02a0e08ff0ebfef9
Eddort <webisforce@gmail.com>
2024-04-22 12:42:11 +0200

    wip: e2e

---
+++ src/anvild.test.ts
@@ -0,0 +1,14 @@
+import { createAnvil } from ".";
+
+describe('base', async () => {
+    it('works', async () => {
+        const node = await createAnvil({
+            anvil: { forkUrl:  "https://cloudflare-eth.com" },
+          });
+
+
+          console.log(node.host, node.port, node.containerId)
+
+          await node.stop()
+    })
+})
